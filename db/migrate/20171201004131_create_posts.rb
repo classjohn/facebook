8f20b72e65f27ae1a53593fb34d1e6931f852425
John Kang <biniprc@hotmail.com>
2017-12-01 07:55:49 +0000

    fix migration

--- db/migrate/20171201004131_create_posts.rb
+++ db/migrate/20171201004131_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title, null: false, default: ""
       t.text :content, null: false, default: ""
-      t.references :user, index: true, foreign_key: true, null: false
+      t.integer :user, index: true
 
       t.timestamps null: false
     end
--- db/migrate/20171201012605_create_comments.rb
+++ db/migrate/20171201012605_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :content, null: false, default: ""
-      t.references :post, index: true, foreign_key: true
-      t.references :user, index: true, foreign_key: true
+      t.integer :post, index: true
+      t.integer :user, index: true
 
       t.timestamps null: false
     end
